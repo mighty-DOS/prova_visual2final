df024bff26042fb02105ea7c4d3aa5650fbf4706
mighty-DOS <h.webprojects@outlook.com>
2021-11-24 11:04:43 -0300

    final upload

---
+++ FRONT/src/app/services/pagamento.service.ts
@@ -0,0 +1,20 @@
+import { Pagamento } from '../models/pagamento';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: "root",
+})
+export class PagamentoService {
+    private baseUrl = "http://localhost:5000/api/pagamento";
+
+    constructor(private http: HttpClient) {}
+
+    list(): Observable<Pagamento[]> {
+        return this.http.get<Pagamento[]>(`${this.baseUrl}/list`);
+    }
+    create(pagamento: Pagamento): Observable<Pagamento> {
+        return this.http.post<Pagamento>(`${this.baseUrl}/create`, pagamento);
+    }
+}

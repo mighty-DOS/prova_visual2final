c3db62d93d3fac3cd73e2b5ac1bbda150b78f213
BrunodeLuca-star <brunodeluca403@gmail.com>
2021-11-24 12:30:02 -0300

    att

--- API/Models/Pagamento.cs
+++ API/Models/Pagamento.cs
@@ -6,9 +6,9 @@ namespace API.Models
     {
         public Pagamento() => CriadoEm = DateTime.Now;
         public int Id { get; set; }
-        public string BandeiraCartao { get; set; }
+        public Usuario Usuario { get; set; }
         public string FormaPagamento { get; set; }
-         public DateTime CriadoEm { get; set; }
+        public DateTime CriadoEm { get; set; }
 
     }
 }
